24a1a3d27755afe958dde7b67df0077479c71c88
Louis Haub <louis.haub@googlemail.com>
2022-01-05 12:47:14 +0100

    fix path

--- config.mk
+++ config.mk
@@ -4,7 +4,7 @@ VERSION = 0.1-alpha
 # Customize below to fit your system
 
 # paths
-SHADER_LOCATION = /home/louis/CompileItYourself/gllock/shaders
+SHADER_LOCATION = /home/louis/.gllock/shaders
 
 # shader
 # FRGMNT_SHADER = ascii.fragment.glsl
